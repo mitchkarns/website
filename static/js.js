b7737b8885f5f2d48c28d8eddd564731adc15578
Mitch <mitchkarns@berkeley.edu>
2016-02-13 15:45:50 -0500

    continuing styling

--- static/js.js
+++ static/js.js
@@ -1,6 +1,11 @@
-$(".nav-link").mouseover(function(){
-    $(this).css("background", "#326589");
-})
-$(".nav-link").mouseout(function(){
-    $(this).css("background", "#2D4759");
+$(document).ready(function(){
+    $(".nav-link").mouseover(function(){
+        $(this).css("background", "#326589");
+    })
+    $(".nav-link").mouseout(function(){
+        $(this).css("background", "#2D4759");
+    })
+    $("#nav-home").attr("href", "http://127.0.0.1:5000/");
+    $("#nav-about").attr("href", "http://127.0.0.1:5000/about");
+    $("#nav-projects").attr("href", "http://127.0.0.1:5000/projects");
 })
